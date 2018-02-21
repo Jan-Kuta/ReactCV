dcd100ae138495108246f178728c59d3c0239445
Jan-Kuta <jan.kuta@email.cz>
2018-02-21 11:06:18 +0100

    Popovers

--- src/main/js/components/skill_list.js
+++ src/main/js/components/skill_list.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SkillList = (props) => { 
-    const skills = props.skills;
-    const renderSkill = (skill) => {
-        const styles={width: `${skill.level}%`};
-        return (
-            <div key={skill.id}>
-                {skill.title} <div className="progress">
-                  <div className="progress-bar bg-warning" style={styles} role="progressbar" aria-valuenow={skill.level} aria-valuemin="0" aria-valuemax="100">{skill.level}%</div>
+class SkillList extends Component{
+    render(){ 
+        const skills = this.props.skills;
+        const renderSkill = (skill) => {
+            const styles={width: `${skill.level}%`};
+            return (
+                <div key={skill.id} className="popover__wrapper">
+                    <div>
+                        {skill.title} <div className="progress">
+                            <div className="progress-bar bg-warning" style={styles} role="progressbar" aria-valuenow={skill.level} aria-valuemin="0" aria-valuemax="100">{skill.level}%</div>
+                        </div>
+                    </div>
+                    <div className="push popover__content">
+                        <div className="popover__message">{skill.note}</div>
+                    </div>
                 </div>
+            );
+        };
+        return (
+            <div>   
+                <h2>Skills</h2>         
+                {skills.map(renderSkill)}
             </div>
         );
-    };
-    return (
-        <div>   
-            <h2>Skills</h2>         
-            {skills.map(renderSkill)}
-        </div>
-    );
+    }
 }
 
 export default SkillList;
