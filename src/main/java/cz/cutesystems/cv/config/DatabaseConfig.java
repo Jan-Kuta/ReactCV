749529c5b7c5a66a7e3987efefce86a1c8165ac6
Jan-Kuta <jan.kuta@email.cz>
2018-02-28 14:40:51 +0100

    For deployment preparation

---
+++ src/main/java/cz/cutesystems/cv/config/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package cz.cutesystems.cv.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by kutik on 28.02.18.
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
